56113e5169fd20b728f6b71614135be5de814192
mallela009 <mallelaarjuna009@gmail.com>
2022-12-04 15:19:22 +0530

    first commit

---
+++ src/main/java/com/simplilearn/FreshMart/FreshMartApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.FreshMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/FreshMart/FreshMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.FreshMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
